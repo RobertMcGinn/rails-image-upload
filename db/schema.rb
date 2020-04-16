797e1ef405bb3d4d0a5f0faf789ebe84040167f4
Robert McGinn <mcginn.robert@gmail.com>
2020-04-16 10:58:22 +0100

    altering javascript tree

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_12_24_110150) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
